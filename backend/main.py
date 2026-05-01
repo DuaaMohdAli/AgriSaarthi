0b748136fc74affa4ccfe461644038fd2b1f8a05
duaa <duamohdali@gmail.com>
2026-05-01 09:37:57 +0530

    fix cors origin typo

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://agrisaarthi.netlify.app"],
+    allow_origins=["https://agrisaarti.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
