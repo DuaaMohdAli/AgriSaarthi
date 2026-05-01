464c16f0d17950fd8df81cdf78a2e37269c05dfb
duaa <duamohdali@gmail.com>
2026-05-01 09:28:01 +0530

    fix cors properly

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://agrisaarthi.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
